d56202542301ae58b023090627ea13049f90f92c
PAI <muhammadfachriadrianta12@gmail.com>
2021-12-22 23:24:49 +0700

    Added

--- src/com/dicoding/javafundamental/basic/Main.java
+++ src/com/dicoding/javafundamental/basic/Main.java
@@ -1,7 +1,9 @@
 package com.dicoding.javafundamental.basic;
 
+import com.dicoding.javafundamental.basic.musik.Gitar;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Gitar.bunyi();
     }
 }
