6e06945872f015c4e81cf7605cbc14bbe6287376
PAI <muhammadfachriadrianta12@gmail.com>
2021-12-24 09:36:35 +0700

    Added

--- src/com/dicoding/javafundamental/basic/dasar/TipeString.java
+++ src/com/dicoding/javafundamental/basic/dasar/TipeString.java
@@ -1,13 +1,22 @@
 package com.dicoding.javafundamental.basic.dasar;
 
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class TipeString {
     public static void tipeString(){
         // String
         String astring = "Hello pai!";
         System.out.println(astring);
+        Integer integer = astring.length();
+        System.out.println("Panjang dari sebuah string " + integer);
 
-        // char
-        char a = 22222;
+        // Array char[]
+        // untuk menghasilkan himpunan berupa huruf maupun angka
+        char[] achar = new char[]{'f','a','c','h','r','i'};
+        String convertachar = String.valueOf(achar);
+        System.out.println("Tipe data char[] (himpunan)");
+        System.out.printf("Char[] himpunan " + convertachar);
 
     }
 }
